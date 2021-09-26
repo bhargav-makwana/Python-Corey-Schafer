3dcdbc02a0b71f0a7c4029aaed541a252475d571
Bhargav <bhargavmakwana706@gmail.com>
2021-09-26 21:54:27 +0200

    added integers and floats file

---
+++ 3-Integers and Floats.py
@@ -0,0 +1,40 @@
+# Mainly two numeric types - Integer(Whole Number) and Float(Decimal)
+import math
+num = 3
+
+# Checking the datatype of the variable - int / float
+print(type(num))
+print(type(math.pi))
+
+# Arithmetic Calculations with user input
+print(5+4) # Addition
+print(5-4) # Subtraction
+print(5*4) # Multiplication
+print(5/4) # Float Division
+print(5//4) # Floor Division
+print(5**4) # Power of the number
+print(5%4) # Modulo
+
+# It always follows PEMDAS rule in the longer arithmetic calculations
+
+num = 10
+num *= 10
+print(num)
+
+# ROUND, ABS value
+print(abs(-10.56))
+print(round(10.345698, 3))
+
+# Comparisons between numbers
+
+num1 = 10
+num2 = -10
+
+print(num1 == num2) # Equal
+print(num1 != num2) # Not Equal
+print(num1 < num2) # Less Than
+print(num1 > num2) # Greater Than
+print(num1 <= num2) # Less Than or Equal
+print(num1 >= num2) # Greater Than or Equal
+
+# Typecasting. 
