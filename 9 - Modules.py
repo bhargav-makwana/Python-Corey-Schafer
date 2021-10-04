d3559566afcfcb875e0e98d469b9be3fb1cb743e
Bhargav <bhargavmakwana706@gmail.com>
2021-10-04 17:55:18 +0200

    Modules

--- 9 - Modules.py
+++ 9 - Modules.py
@@ -1,4 +1,5 @@
 import my_module
+import sys
 
 courses = ['ABC', 'CDE', 'EFG']
 
@@ -6,4 +7,7 @@ index = my_module.find_index(courses, 'CDE')
 
 print(index)
 print(my_module.test)
+print(sys.path)
 
+# Check the video to set the path of importing module
+# Credits : https://www.youtube.com/watch?v=CqvZ3vGoGs0&list=PL-osiE80TeTskrapNbzXhwoFUiLCjGgY7&index=9&ab_channel=CoreySchafer
